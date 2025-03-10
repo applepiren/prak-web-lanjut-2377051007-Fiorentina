83b6cbc4454dff49fa2edf99e339e39083ec2328
applepiren <143419596+applepiren@users.noreply.github.com>
2025-03-10 10:04:22 +0700

    menambah profile

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
 });
+Route::get('/profile', [ProfileController::class, 'profile']);
+Route::get('/profile/{nama}/{kelas}/{npm}', [ProfileController::class, 'profile']);
+
+
