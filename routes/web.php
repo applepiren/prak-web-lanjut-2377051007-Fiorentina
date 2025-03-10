66781b4a5b7d670385ea68dff28a12c5ee80e4f8
applepiren <143419596+applepiren@users.noreply.github.com>
2025-03-10 11:14:25 +0700

    menambah form

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -20,5 +21,6 @@ Route::get('/', function () {
 });
 Route::get('/profile', [ProfileController::class, 'profile']);
 Route::get('/profile/{nama}/{kelas}/{npm}', [ProfileController::class, 'profile']);
-
+Route::get('/user/create', [UserController::class, 'create'])->name('user_create');
+Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
 
